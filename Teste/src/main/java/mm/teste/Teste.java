ecf3c9a26e583b8676b219734c6c06892e602907
mateu <mateu@LAPTOP-KD9LPAJO>
2023-06-14 19:47:14 -0300

    testando

---
+++ Teste/src/main/java/mm/teste/Teste.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mm.teste;
+
+/**
+ *
+ * @author mateu
+ */
+public class Teste {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
